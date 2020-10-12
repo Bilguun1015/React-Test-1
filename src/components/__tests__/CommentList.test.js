aa9d8a175c4a7127d738e3763ab8c13d376d43dd
Bilguun Nomch <bbg.1015@gmail.com>
2020-10-12 16:09:30 -0500

    modified Root to take initial state to test

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -5,10 +5,15 @@ import CommentList from 'components/CommentList';
 
 let wrapped;
 beforeEach(() => {
+  const initialState = {
+    comments: ['Comment 1', 'Comment 2'],
+  };
   wrapped = mount(
-    <Root>
+    <Root initialState={initialState}>
       <CommentList />
     </Root>
   );
 });
-it('creates one LI per comment', () => {});
+it('creates one LI per comment', () => {
+  console.log(wrapped.find('li').length);
+});

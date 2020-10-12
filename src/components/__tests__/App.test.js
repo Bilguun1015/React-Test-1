acd89875ec3524650aa8b89a25e79b9a9bd976d6
Bilguun Nomch <bbg.1015@gmail.com>
2020-10-12 12:15:00 -0500

    added absolute imports

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import App from '../App';
-import CommentBox from '../CommentBox';
+import { shallow, mount } from 'enzyme';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
 
 it('shows a comment box', () => {
   const wrapped = shallow(<App />);
 
   expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it('shows a comment list', () => {
+  const wrapped = mount(<App />);
+
+  expect(wrapped.find(CommentList).exists()).toEqual(true);
 });

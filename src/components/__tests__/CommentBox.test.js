e06d9f84cff257407c0207663a48c20a4b187e1c
Bilguun Nomch <bbg.1015@gmail.com>
2020-10-12 12:54:11 -0500

    added some test for CommentBox

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,6 +1,15 @@
 import React from 'react';
-
+import { mount } from 'enzyme';
 import CommentBox from 'components/CommentBox';
-import { idText } from 'typescript';
 
-it('has a text area and a button', () => {});
+let wrapped;
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+
+it('has a text area and a button', () => {
+  expect(wrapped.find('textarea').length).toEqual(1);
+  expect(wrapped.find('button').length).toEqual(1);
+});
+
+// console.log(wrapped.find("textarea").length);

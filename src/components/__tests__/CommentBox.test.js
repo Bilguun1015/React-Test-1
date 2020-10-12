6b8bba21d677b9df4d8bda4c8cb4d0b9a94bcc00
Bilguun Nomch <bbg.1015@gmail.com>
2020-10-12 13:10:45 -0500

    simulating onChange callback funtion

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -7,9 +7,19 @@ beforeEach(() => {
   wrapped = mount(<CommentBox />);
 });
 
+afterEach(() => {
+  wrapped.unmount();
+});
+
 it('has a text area and a button', () => {
+  // console.log(wrapped.find("textarea").length);
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
 });
 
-// console.log(wrapped.find("textarea").length);
+it('has a text area that users can type in', () => {
+  wrapped.find('textarea').simulate('change', {
+    target: {value: 'new comment'}
+  })
+})
+

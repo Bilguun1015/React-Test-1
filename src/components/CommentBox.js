2406fc13c7e9636973f48c42fff039a02b1851c6
Bilguun Nomch <bbg.1015@gmail.com>
2020-10-12 12:33:55 -0500

    changed CommentBox component nto a class component and added form

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-  return <div>Comment Box</div>;
-};
+class CommentBox extends Component {
+  state = { comment: '' };
+
+  handleChange = (event) => {
+    this.setState({ comment: event.target.value });
+  };
+
+  render() {
+    return (
+      <form>
+        <h4>Add a Comment</h4>
+        <textarea value={this.state.comment} onChange={this.handleChange} />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default CommentBox;

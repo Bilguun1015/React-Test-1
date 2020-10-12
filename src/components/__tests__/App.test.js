08359c1c2d0c4468f4fbda4c427024c01ac4c0a1
Bilguun Nomch <bbg.1015@gmail.com>
2020-10-12 12:22:44 -0500

    added beforeEach function to setup common patterns before the test runs

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import App from 'components/App';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
-it('shows a comment box', () => {
-  const wrapped = shallow(<App />);
+// common setup before each test runs
+let wrapped;
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
 
+it('shows a comment box', () => {
   expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it('shows a comment list', () => {
-  const wrapped = mount(<App />);
-
-  expect(wrapped.find(CommentList).exists()).toEqual(true);
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });

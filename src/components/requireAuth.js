b848ed2580e7569ce51106513bed258bb1edb9cf
Bilguun Nomch <bbg.1015@gmail.com>
2020-10-23 17:59:16 -0500

    moved the reusable authentication logic to the higher order component

--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export default (ChildComponent) => {
   class ComposedComponent extends Component {
+    // component just got rendered
+    componentDidMount() {
+      this.shouldNavigateAway();
+    }
+
+    // component just got updated
+    componentDidUpdate() {
+      this.shouldNavigateAway();
+    }
+
+    shouldNavigateAway = () => {
+      if (!this.props.auth) {
+        this.props.history.push('/');
+      }
+    };
     render() {
       return <ChildComponent />;
     }
   }
 
-  return ComposedComponent;
+  function mapStateToProps(state) {
+    return { auth: state.auth };
+  }
+
+  return connect(mapStateToProps)(ComposedComponent);
 };

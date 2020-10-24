51f7541246b0934dde6d58a5ce1ca8dae1b76225
Bilguun Nomch <bbg.1015@gmail.com>
2020-10-24 11:38:21 -0500

    working through the logic of setting async middleware

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,1 +1,9 @@
-export default ({ dispatch }) => (next) => (action) => {};
+export default ({ dispatch }) => (next) => (action) => {
+  // Check to see if the action
+  // has a promise on its 'payload' property
+  // if it does, then wait for it to resolve
+  // if it doesn't, then send the action on to the
+  // next middleware
+
+  if (!action.payload || !action.payload.then) return next(action);
+};

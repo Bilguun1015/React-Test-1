4ca8d0a12f2f633e95bef739ebdd54819e846802
Bilguun Nomch <bbg.1015@gmail.com>
2020-10-25 11:40:57 -0500

    created schema for redux store and validation

--- src/middlewares/stateValidator.js
+++ src/middlewares/stateValidator.js
@@ -1,1 +1,8 @@
-export default ({ dispatch }) => (next) => (action) => {};
+import tv4 from 'tv4';
+import stateSchema from './stateSchema';
+
+export default ({ dispatch, getState }) => (next) => (action) => {
+  next(action);
+
+  tv4.validate(getState(), stateSchema);
+};
